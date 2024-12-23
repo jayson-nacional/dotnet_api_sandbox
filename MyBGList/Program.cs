693ced2eb1b3e37e968e86a34efd731d25d96cd8
jayson-nacional <jayson.o.nacional@gmail.com>
2024-12-23 12:33:25 +0800

    Error handling on the request pipeline

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -23,10 +23,18 @@ public class Program
             app.UseSwaggerUI();
         }
 
+		if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+			app.UseDeveloperExceptionPage();
+		else
+			app.UseExceptionHandler("/error");
+
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
 
+		app.MapGet("/error", () => Results.Problem());
+
+		app.MapGet("/error/test", () => { throw new Exception("test"); });
 
         app.MapControllers();
 
