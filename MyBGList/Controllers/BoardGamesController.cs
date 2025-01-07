09f8ab8257306b2c8889fa057f2b51d59f876301
jayson-nacional <jayson.o.nacional@gmail.com>
2025-01-07 21:08:14 +0800

    DTOs

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MyBGList.DTO;
 
 namespace MyBGList.Controllers;
 
@@ -14,34 +15,44 @@ public class BoardGamesController : ControllerBase
     }
 
     [HttpGet(Name = "GetBoardGames")]
-    public IEnumerable<BoardGame> Get()
+    public RestDTO<BoardGame[]> Get()
     {
-        return new[]
+        return new RestDTO<BoardGame[]>()
         {
-            new BoardGame()
+            Data = new BoardGame[]
             {
-                Id = 1,
-                Name = "Axis & Allies",
-                Year = 1981,
-				MinPlayers = 2,
-				MaxPlayers = 5
+                new BoardGame()
+                {
+                    Id = 1,
+                    Name = "Axis & Allies",
+                    Year = 1981,
+                    MinPlayers = 2,
+                    MaxPlayers = 5
+                },
+                new BoardGame()
+                {
+                    Id = 2,
+                    Name = "Citadels",
+                    Year = 2000,
+                    MinPlayers = 2,
+                    MaxPlayers = 8
+                },
+                new BoardGame()
+                {
+                    Id = 3,
+                    Name = "Terraforming Mars",
+                    Year = 2016,
+                    MinPlayers = 1,
+                    MaxPlayers = 5
+                }
             },
-            new BoardGame()
-            {
-                Id = 2,
-                Name = "Citadels",
-                Year = 2000,
-				MinPlayers = 2,
-				MaxPlayers = 8
-            },
-            new BoardGame()
-            {
-                Id = 3,
-                Name = "Terraforming Mars",
-                Year = 2016,
-				MinPlayers = 1,
-				MaxPlayers = 5
-            }
+			Links = new List<LinkDTO>
+			{
+				new LinkDTO(
+						Url.Action(null, "BoardGames", null, Request.Scheme)!,
+						"self",
+						"GET"),
+			}
         };
     }
 }
